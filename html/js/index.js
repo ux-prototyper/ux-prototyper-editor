70a8942e0e00b2cd412e398da4fd52ea179138c1
Evgenii Sopov <mrseakg@gmail.com>
2020-07-21 23:19:27 +0700

    Redesign and refactoring code

--- html/js/index.js
+++ html/js/index.js
@@ -1,9 +1,8 @@
 
-function loadYamlFile(url, func) {
+/*function loadYamlFile(url, func) {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.send();
-  
     request.onreadystatechange = function() {
       if (this.readyState != 4) return;
     
@@ -13,7 +12,7 @@ function loadYamlFile(url, func) {
       }
       func(this.responseText);
     }
-}
+}*/
 
 function loadComponents() {
   var el = document.getElementById('list_components');
@@ -29,14 +28,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
   loadComponents();
   // TODO scroll
   // loadComponents()
-  // loadComponents()
-  // loadComponents()
-  initCanvas();
+  window.viewEditor = new UXProtoEditor("show_design");
+  yamlText = document.getElementById('yaml_conf').innerHTML
+  window.proj = YAML.parse(yamlText);
+  // console.log(example);
+  document.getElementById('project_name').innerHTML = proj.project.name
+  document.getElementById('project_version').innerHTML = proj.project.version
+  document.getElementById('project_views').innerHTML = "";
+  for (var i in proj.views) {
+    var v = proj.views[i];
+    document.getElementById('project_views').innerHTML
+      += '<div class="uxp-proj-view" onclick="click_load_view(this);" view-id="' + v.id + '">' + v.title + '</div>';
+  }
 
-  loadYamlFile("./example.yaml", function(yamlText) {
+  /*loadYamlFile("./example.yaml", function(yamlText) {
     console.log(yamlText);
     nativeObject = YAML.parse(yamlText);
     console.log(nativeObject);
       // TODO apply
-  });
+  });*/
 });
+
+
+function switch_to_editor() {
+  document.getElementById('tab_yaml').classList.remove('active');
+  document.getElementById('tab_view_editor').classList.add('active');
+
+  document.getElementById('show_design').style.display = '';
+  document.getElementById('yaml_conf').style.display = 'none';
+}
+
+function switch_to_yaml() {
+  document.getElementById('tab_yaml').classList.add('active');
+  document.getElementById('tab_view_editor').classList.remove('active');
+
+  document.getElementById('show_design').style.display = 'none';
+  document.getElementById('yaml_conf').style.display = '';
+}
+
+function click_load_view(e) {
+  var viewid = e.getAttribute("view-id");
+  for (var i in window.proj.views) {
+    var v = window.proj.views[i]
+    if (v.id == viewid) {
+      window.viewEditor.apply(v);
+    }
+  }
+}
--- html/js/renderer.js
+++ html/js/renderer.js
@@ -1,248 +1,250 @@
 
-window.r_squares = [
-  {
-    x: 100,
-    y: 100,
-    width: 100,
-    height: 100,
-    selected: true
-  }, 
+function UXProtoEditor(elemId) {
+  var self = this;
+  self.components = [];
+  self.canvasId = elemId;
+  self.canvas = document.getElementById(elemId);
+  self.touchPointsSize = 8
+  self.allowDown = false;
+  self.resizeObj = null;
+  self.doMovingObj = false;
 
-  {
-    x: 230,
-    y: 230,
-    width: 100,
-    height: 100,
-    selected: false
-  }, 
-]
+  self.render = function() {
+    var ctx = self.canvas.getContext('2d');
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.fillRect(0, 0, self.canvas.width, self.canvas.height);
+    ctx.strokeRect(0, 0, self.canvas.width, self.canvas.height);
 
-window.touchPointsSize = 8
-
-function renderSchema(canvas) {
-  var ctx = canvas.getContext('2d');
-  ctx.lineWidth = 1;
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-  // NOT selected componnets rendering
-  ctx.lineWidth = 1;
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  for (var i in r_squares) {
-    var obj = r_squares[i]
-    if (!obj.selected) {
-      ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
-      ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
-    }
-  }
-
-  // selected components rendering
-  for (var i in r_squares) {
-    var obj = r_squares[i]
-    if (obj.selected) {
-      ctx.lineWidth = 1;
-      ctx.fillStyle = 'rgb(200, 200, 200)';
-      ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
-      ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
-
-      ctx.fillStyle = 'rgb(100, 100, 100)';
-
-      ctx.fillRect(obj.x - touchPointsSize/2, obj.y - touchPointsSize/2, touchPointsSize, touchPointsSize);
-      ctx.strokeRect(obj.x - touchPointsSize/2, obj.y - touchPointsSize/2, touchPointsSize, touchPointsSize);
-
-      ctx.fillRect(obj.x + obj.width - touchPointsSize/2, obj.y - touchPointsSize/2, touchPointsSize, touchPointsSize);
-      ctx.strokeRect(obj.x + obj.width - touchPointsSize/2, obj.y - touchPointsSize/2, touchPointsSize, touchPointsSize);
-
-      ctx.fillRect(obj.x + obj.width - touchPointsSize/2, obj.y + obj.height - touchPointsSize/2, touchPointsSize, touchPointsSize);
-      ctx.strokeRect(obj.x + obj.width - touchPointsSize/2, obj.y + obj.height - touchPointsSize/2, touchPointsSize, touchPointsSize);
-
-      ctx.fillRect(obj.x - touchPointsSize/2, obj.y + obj.height - touchPointsSize/2, touchPointsSize, touchPointsSize);
-      ctx.strokeRect(obj.x - touchPointsSize/2, obj.y + obj.height - touchPointsSize/2, touchPointsSize, touchPointsSize);
-    }
-  }
-}
-
-window.allowDown = false;
-window.resizeObj = null;
-window.doMovingObj = false;
-
-function canvasOnMouseMove(e) {
-  event = e || window.event; // IE-ism
-
-  // e = Mouse click event.
-  var rect = e.target.getBoundingClientRect();
-  var x = e.clientX - rect.left; //x position within the element.
-  var y = e.clientY - rect.top;  //y position within the element
-  // console.warn(x, y);
-
-  if (window.doMovingObj) {
-    if (window.resizeObj != null) {
-      var obj = r_squares[resizeObj.id];
-      if (window.resizeObj.how == 'move') {
-        obj.x = x - resizeObj.diffX;
-        obj.y = y - resizeObj.diffY;
-        renderSchema(e.target);
-        return;
-      }
-      if (window.resizeObj.how == 'top-left') {
-        obj.width = obj.x + obj.width - x;
-        obj.x = x;
-        obj.height = obj.y + obj.height - y;
-        obj.y = y;
-        renderSchema(e.target);
-        return;
-      }
-      if (window.resizeObj.how == 'top-right') {
-        obj.width = x - obj.x;
-        obj.height = obj.y + obj.height - y;
-        obj.y = y;
-        renderSchema(e.target);
-        return;
-      }
-
-      if (window.resizeObj.how == 'bottom-left') {
-        obj.width = obj.x + obj.width - x;
-        obj.x = x;
-        obj.height = y - obj.y;
-        renderSchema(e.target);
-        return;
-      }
-
-      if (window.resizeObj.how == 'bottom-right') {
-        obj.width = x - obj.x;
-        obj.height = y - obj.y;
-        renderSchema(e.target);
-        return;
+    // NOT selected componets rendering
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    for (var i in self.components) {
+      var obj = self.components[i]
+      if (!obj.selected) {
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+        ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
       }
     }
-    return;
-  }
 
-  cursor = "default";
-  window.allowDown = false;
-  window.resizeObj = null;
-  for (var i in r_squares) {
-    var obj = r_squares[i];
-    if (obj.selected) {
-      // move
-      if (
-        y >= obj.y && y <= obj.y + obj.height
-        && x >= obj.x && x <= obj.x + obj.width
-      ) {
-        cursor = "move";
-        window.allowDown = true;
-        window.resizeObj = { id: i, how: 'move'};
-      }
+    // selected components rendering
+    for (var i in self.components) {
+      var obj = self.components[i]
+      if (obj.selected) {
+        ctx.lineWidth = 1;
+        ctx.fillStyle = 'rgb(200, 200, 200)';
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+        ctx.strokeRect(obj.x, obj.y, obj.width, obj.height);
 
-      // top left 
-      if (
-        y >= obj.y - touchPointsSize/2 && y <= obj.y + touchPointsSize
-        && x >= obj.x - touchPointsSize/2 && x <= obj.x + touchPointsSize
-      ) {
-        cursor = "nwse-resize";
-        window.allowDown = true;
-        window.resizeObj = { id: i, how: 'top-left'};
-      }
+        ctx.fillStyle = 'rgb(100, 100, 100)';
 
-      // top right
-      if (
-        y >= obj.y - touchPointsSize/2 && y <= obj.y + touchPointsSize
-        && x >= obj.x + obj.width - touchPointsSize/2 && x <= obj.x + obj.width + touchPointsSize
-      ) {
-        cursor = "nesw-resize";
-        window.allowDown = true;
-        window.resizeObj = { id: i, how: 'top-right'};
-      }
+        ctx.fillRect(obj.x - self.touchPointsSize/2, obj.y - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
+        ctx.strokeRect(obj.x - self.touchPointsSize/2, obj.y - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
 
-      // bottom left
-      if (
-        y >= obj.y + obj.height - touchPointsSize/2 && y <= obj.y + obj.height + touchPointsSize
-        && x >= obj.x - touchPointsSize/2 && x <= obj.x + touchPointsSize
-      ) {
-        cursor = "nesw-resize";
-        window.allowDown = true;
-        window.resizeObj = { id: i, how: 'bottom-left'};
-      }
-      
-      // bottom right
-      if (
-        y >= obj.y + obj.height - touchPointsSize/2 && y <= obj.y + obj.height + touchPointsSize
-        && x >= obj.x + obj.width - touchPointsSize/2 && x <= obj.x + obj.width + touchPointsSize
-      ) {
-        cursor = "nwse-resize";
-        window.allowDown = true;
-        window.resizeObj = { id: i, how: 'bottom-right'};
-      }
+        ctx.fillRect(obj.x + obj.width - self.touchPointsSize/2, obj.y - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
+        ctx.strokeRect(obj.x + obj.width - self.touchPointsSize/2, obj.y - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
 
-      if (window.allowDown) {
-        e.target.style.cursor = cursor;
-        return;
+        ctx.fillRect(obj.x + obj.width - self.touchPointsSize/2, obj.y + obj.height - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
+        ctx.strokeRect(obj.x + obj.width - self.touchPointsSize/2, obj.y + obj.height - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
+
+        ctx.fillRect(obj.x - self.touchPointsSize/2, obj.y + obj.height - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
+        ctx.strokeRect(obj.x - self.touchPointsSize/2, obj.y + obj.height - self.touchPointsSize/2, self.touchPointsSize, self.touchPointsSize);
       }
     }
   }
 
-  for (var i in r_squares) {
-    var obj = r_squares[i];
-    if (!obj.selected) {
-      // select
-      if (
-        y >= obj.y && y <= obj.y + obj.height
-        && x >= obj.x && x <= obj.x + obj.width
-      ) {
-        window.allowDown = true;
-        e.target.style.cursor = "pointer";
-        window.resizeObj = { id: i, how: 'can-select'};
-        return;
-      }
+  self.apply = function(v) {
+    self.view_title = v.title;
+    self.view_id = v.id;
+    self.components = [];
+    for (var i in v.components) {
+      var c = v.components[i];
+      self.components.push({
+        id: c.id, 
+        type: c.type,
+        x: 100,
+        y: 100,
+        width: 100,
+        height: 100,
+        selected: false
+      })
     }
+    self.render();
   }
-  e.target.style.cursor = "default";
-}
 
-function canvasOnMouseDown(e) {
-  event = e || window.event; // IE-ism
-  var rect = e.target.getBoundingClientRect();
-  var x = e.clientX - rect.left; //x position within the element.
-  var y = e.clientY - rect.top;  //y position within the element
-
-  if (window.allowDown) {
-    if (window.resizeObj != null) {
-      var obj = r_squares[resizeObj.id];
-      if (resizeObj.how == 'can-select') {
-        for (var i in r_squares) {
-          r_squares[i].selected = false;
+  self.canvasOnMouseMove = function(e) {
+    event = e || window.event; // IE-ism
+  
+    // e = Mouse click event.
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element
+    // console.warn(x, y);
+  
+    if (self.doMovingObj) {
+      if (self.resizeObj != null) {
+        var obj = self.components[self.resizeObj.id];
+        if (self.resizeObj.how == 'move') {
+          obj.x = x - self.resizeObj.diffX;
+          obj.y = y - self.resizeObj.diffY;
+          self.render();
+          return;
         }
-        obj.selected = true;
-        window.resizeObj.how = 'move';
-        renderSchema(e.target);
+        if (self.resizeObj.how == 'top-left') {
+          obj.width = obj.x + obj.width - x;
+          obj.x = x;
+          obj.height = obj.y + obj.height - y;
+          obj.y = y;
+          self.render(e.target);
+          return;
+        }
+        if (self.resizeObj.how == 'top-right') {
+          obj.width = x - obj.x;
+          obj.height = obj.y + obj.height - y;
+          obj.y = y;
+          self.render(e.target);
+          return;
+        }
+  
+        if (self.resizeObj.how == 'bottom-left') {
+          obj.width = obj.x + obj.width - x;
+          obj.x = x;
+          obj.height = y - obj.y;
+          self.render();
+          return;
+        }
+  
+        if (self.resizeObj.how == 'bottom-right') {
+          obj.width = x - obj.x;
+          obj.height = y - obj.y;
+          self.render();
+          return;
+        }
       }
-      window.doMovingObj = true;
-      resizeObj.diffX = x - obj.x
-      resizeObj.diffY = y - obj.y
+      return;
+    }
+  
+    cursor = "default";
+    self.allowDown = false;
+    self.resizeObj = null;
+    for (var i in self.components) {
+      var obj = self.components[i];
+      if (obj.selected) {
+        // move
+        if (
+          y >= obj.y && y <= obj.y + obj.height
+          && x >= obj.x && x <= obj.x + obj.width
+        ) {
+          cursor = "move";
+          self.allowDown = true;
+          self.resizeObj = { id: i, how: 'move'};
+        }
+  
+        // top left 
+        if (
+          y >= obj.y - self.touchPointsSize/2 && y <= obj.y + self.touchPointsSize
+          && x >= obj.x - self.touchPointsSize/2 && x <= obj.x + self.touchPointsSize
+        ) {
+          cursor = "nwse-resize";
+          self.allowDown = true;
+          self.resizeObj = { id: i, how: 'top-left'};
+        }
+  
+        // top right
+        if (
+          y >= obj.y - self.touchPointsSize/2 && y <= obj.y + self.touchPointsSize
+          && x >= obj.x + obj.width - self.touchPointsSize/2 && x <= obj.x + obj.width + self.touchPointsSize
+        ) {
+          cursor = "nesw-resize";
+          self.allowDown = true;
+          self.resizeObj = { id: i, how: 'top-right'};
+        }
+  
+        // bottom left
+        if (
+          y >= obj.y + obj.height - self.touchPointsSize/2 && y <= obj.y + obj.height + self.touchPointsSize
+          && x >= obj.x - self.touchPointsSize/2 && x <= obj.x + self.touchPointsSize
+        ) {
+          cursor = "nesw-resize";
+          self.allowDown = true;
+          self.resizeObj = { id: i, how: 'bottom-left'};
+        }
+        
+        // bottom right
+        if (
+          y >= obj.y + obj.height - self.touchPointsSize/2 && y <= obj.y + obj.height + self.touchPointsSize
+          && x >= obj.x + obj.width - self.touchPointsSize/2 && x <= obj.x + obj.width + self.touchPointsSize
+        ) {
+          cursor = "nwse-resize";
+          self.allowDown = true;
+          self.resizeObj = { id: i, how: 'bottom-right'};
+        }
+  
+        if (self.allowDown) {
+          e.target.style.cursor = cursor;
+          return;
+        }
+      }
+    }
+  
+    for (var i in self.components) {
+      var obj = self.components[i];
+      if (!obj.selected) {
+        // select
+        if (
+          y >= obj.y && y <= obj.y + obj.height
+          && x >= obj.x && x <= obj.x + obj.width
+        ) {
+          self.allowDown = true;
+          e.target.style.cursor = "pointer";
+          self.resizeObj = { id: i, how: 'can-select'};
+          return;
+        }
+      }
+    }
+    e.target.style.cursor = "default";
+  }
+
+  self.canvasOnMouseDown = function(e) {
+    event = e || window.event; // IE-ism
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element
+
+    if (self.allowDown) {
+      if (self.resizeObj != null) {
+        var obj = self.components[self.resizeObj.id];
+        if (self.resizeObj.how == 'can-select') {
+          for (var i in self.components) {
+            self.components[i].selected = false;
+          }
+          obj.selected = true;
+          self.resizeObj.how = 'move';
+          self.render(e.target);
+        }
+        self.doMovingObj = true;
+        self.resizeObj.diffX = x - obj.x
+        self.resizeObj.diffY = y - obj.y
+      }
     }
   }
-}
 
-function canvasOnMouseUp(e) {
-  event = e || window.event; // IE-ism
-  var rect = e.target.getBoundingClientRect();
-  var x = e.clientX - rect.left; //x position within the element.
-  var y = e.clientY - rect.top;  //y position within the element
+  self.canvasOnMouseUp = function(e) {
+    event = e || window.event; // IE-ism
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element
 
-  if (window.doMovingObj) {
-    if (window.resizeObj != null) {
-      var obj = r_squares[resizeObj.id];
-      window.doMovingObj = false;
-      resizeObj = null;
+    if (self.doMovingObj) {
+      if (self.resizeObj != null) {
+        var obj = self.components[self.resizeObj.id];
+        self.doMovingObj = false;
+        self.resizeObj = null;
+      }
     }
   }
-}
 
-function initCanvas() {
-  var canvas = document.getElementById("show_design");
-  renderSchema(canvas)
-  canvas.onmousemove = canvasOnMouseMove;
-  canvas.onmousedown = canvasOnMouseDown;
-  canvas.onmouseup = canvasOnMouseUp;
+  self.canvas.onmousemove = self.canvasOnMouseMove;
+  self.canvas.onmousedown = self.canvasOnMouseDown;
+  self.canvas.onmouseup = self.canvasOnMouseUp;
 }
